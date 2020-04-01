a267f477976a0008f75196c56a2971d537c8441c
nataliaMarzec <natimarzec708@gmail.com>
2020-04-01 12:33:25 -0300

    mongo-md

---
+++ backend/server.js
@@ -0,0 +1,68 @@
+express = require("express");
+bodyParser = require("body-parser");
+var cors = require('cors');
+
+var homes = {}
+
+
+function register(home) {
+  console.log(`registering handlers for ${home.type}`)
+  homes[home.type] = home 
+}
+
+function init() {
+  var server = express();
+  server.use(bodyParser.json());
+
+  server.use("(/:type/*)|(/:type)", (req, res, next) => {
+      if (!homes[req.params.type]) {
+          console.log(` home de ${req.params.type} no existe`  )
+          res.status(404).end()
+      }
+      else {
+        console.log(` home de ${req.params.type} si existe `  )
+        next()
+      }
+  })
+
+  server.use(cors())
+
+  server.get("/:type", (req, res) => {
+    home = homes[req.params.type]
+    home.all((allObjects) => {
+        res.json(allObjects) 
+        res.end() })       
+  })
+
+  server.get("/:type/:id", (req, res) => {
+    home = homes[req.params.type]
+    home.get(req.params.id, (myObject) => { 
+      res.json(myObject) 
+      res.end() })  
+  })
+
+  server.put("/:type", (req, res) => {
+    home = homes[req.params.type]
+    home.update(req.body)
+    res.status(204).end();  
+  })
+
+  server.post("/:type", (req, res) => {
+    home = homes[req.params.type]
+    home.insert(req.body)
+    res.status(204).end();  
+  })
+
+  server.delete("/:type/:id", (req, res) => {
+    home = homes[req.params.type]
+    home.delete(req.params.id)
+    res.status(204).end();  
+  });
+
+  server.listen(8888, () => {
+    console.log("Server running on port 8888");
+  });
+}
+
+exports.init = init;
+exports.register = register;
